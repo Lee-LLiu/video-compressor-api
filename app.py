7d3c7dc741c6ac89de27138807481179d7304f59
Lee-LLiu <leeliupurpledon@gmail.com>
2026-06-24 14:16:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,38 +15,30 @@ def home():
 
 @app.route("/upload", methods=["POST"])
 def upload():
-
     if "video" not in request.files:
-       return jsonify({
-           "success": False,
-           "message": "没有收到视频"
+        return jsonify({
+            "success": False,
+            "message": "没有收到视频"
+        })
+
+    file = request.files["video"]
+    uid = str(uuid.uuid4())
+    filename = uid + "_" + file.filename
+    filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+    file.save(filepath)
+    filesize = os.path.getsize(filepath)
+
+    return jsonify({
+        "success": True,
+        "message": "上传成功",
+        "filename": file.filename,
+        "saved_path": filepath,
+        "size_bytes": filesize
     })
-
-file = request.files["video"]
-
-uid = str(uuid.uuid4())
-
-filename = uid + "_" + file.filename
-
-filepath = os.path.join(
-    UPLOAD_FOLDER,
-    filename
-)
-
-file.save(filepath)
-
-filesize = os.path.getsize(filepath)
-
-return jsonify({
-    "success": True,
-    "message": "上传成功",
-    "filename": file.filename,
-    "saved_path": filepath,
-    "size_bytes": filesize
-})
 
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=10000
-)
+    )
